1dc781d0964b57bec3bdda79c6da9251c180429b
kratun <mitevdv@abv.bg>
2020-07-27 23:31:42 +0300

    Add summaries in IRead and ReadService

--- GreenVsRed/Services/IRead.cs
+++ GreenVsRed/Services/IRead.cs
@@ -1,7 +1,18 @@
-﻿namespace GreenVsRed.Services
+﻿// <copyright file="IRead.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GreenVsRed.Services
 {
+    /// <summary>
+    /// Provide read methods.
+    /// </summary>
     public interface IRead
     {
+        /// <summary>
+        /// Read a single line.
+        /// </summary>
+        /// <returns>Read a single line and return it's trimmed value.</returns>
         string ReadLine();
     }
 }
--- GreenVsRed/Services/ReadService.cs
+++ GreenVsRed/Services/ReadService.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="ReadService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GreenVsRed.Services
 {
+    using System;
+
+    /// <summary>
+    /// Provide read methods.
+    /// </summary>
+    /// <inheritdoc cref="IRead"/>
     public class ReadService : IRead
     {
+        /// <summary>
+        /// Read a single line.
+        /// </summary>
+        /// <returns>Read a single line and return it's trimmed value.</returns>
         public string ReadLine()
         {
             return Console.ReadLine().Trim();
